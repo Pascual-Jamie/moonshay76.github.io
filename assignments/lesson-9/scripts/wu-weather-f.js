85a610b4340ee0d6894c83a74df888a04e8d1266
moonshay76 <moonshay76@gmail.com>
2018-03-10 22:14:22 -0500

    L09 take 5

--- assignments/lesson-9/scripts/wu-weather-f.js
+++ assignments/lesson-9/scripts/wu-weather-f.js
@@ -3,42 +3,50 @@
 var weatherFranklin = new XMLHttpRequest();
 
 weatherFranklin.open('GET', 'http://api.wunderground.com/api/1f7ee438ba530e58/conditions/q/MN/Franklin.json', true);
-                     
+
 weatherFranklin.send();
 
-weatherFranklin.onload = function() {
-    
+weatherFranklin.onload = function () {
+
     var weatherInfo = JSON.parse(weatherFranklin.responseText);
     console.log(weatherInfo);
-    
+
     document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.city;
-    
+
     document.getElementById('highTemp').innerHTML = weatherInfo.current_observation.temp_f;
-    
+
     document.getElementById('imageIcon').src = weatherInfo.current_observation.icon_url;
-        
+
     document.getElementById('precipitate').innerHTML = weatherInfo.current_observation.precip_1hr_in;
-    
+
     document.getElementById('windSpeed').innerHTML = weatherInfo.current_observation.wind_mph;
-    
-    document.getElementById('forcast').innerHTML = weatherInfo.current_observation.weather;    
-    
+
+    document.getElementById('forcast').innerHTML = weatherInfo.current_observation.weather;
+
+    document.getElementById('winDir').innerHTML = weatherInfo.current_observation.wind_dir;
+
     document.getElementById('windOutput').innerHTML = weatherInfo.current_observation.windchill_string;
-    
+
+
+
 } //end of onload
 
 
 // Current Conditions for Franklin
 
+
+
 var currentW = new XMLHttpRequest();
 
-currentW.open('GET','http://api.wunderground.com/api/1f7ee438ba530e58/forecast/q/MN/Franklin.json', true);
+currentW.open('GET', 'http://api.wunderground.com/api/1f7ee438ba530e58/forecast/q/MN/Franklin.json', true);
 currentW.send();
 
-currentW.onload = funcion() {
+currentW.onload = function () {
+
     var weatherI = JSON.parse(currentW.responseText);
     console.log(weatherI);
-    
-    document.getElementById('weatherCurrent').innerHTML = weatherI.forecast.txt_forecast.forecastday["0"].fcttext;    
-    
+
+    document.getElementById('weatherCurrent').innerHTML = weatherI.forecast.txt_forecast.forecastday["0"].fcttext;
+
+
 }
